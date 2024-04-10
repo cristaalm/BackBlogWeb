50c7e4c60e93d3a691f8d5244fec99d8cb8b72d6
Cristal Medina <bmedina1@ucol.mx>
2024-04-10 17:38:15 -0600

    server2

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const corsOptions = {
 // app.use(cors(corsOptions));
 app.use(cors());
 
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     limit: "50mb",
     extended: true,
     parameterLimit: 50000,
